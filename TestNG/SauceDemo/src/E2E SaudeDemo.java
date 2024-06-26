2617d5d9e16e43c7b4688d2c6591c01eef5c612c
Ivan A <hivanarmadi@gmail.com>
2024-06-26 11:38:26 +0700

    TestNG- V.1.0.SauceDemo

---
+++ TestNG/SauceDemo/src/E2E SaudeDemo.java
@@ -0,0 +1,33 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sellntroduction {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("user-name")).sendKeys("standard_user"); //CSS used ID		
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");		
+		driver.findElement(By.className("btn_action")).click();	//CSS used class	
+		
+		driver.findElement(By.xpath("//button[@name='add-to-cart-sauce-labs-backpack']")).click(); //Used XPath generic syntax
+		driver.findElement(By.cssSelector("button[id='add-to-cart-sauce-labs-bike-light']")).click(); //Used CSS generic syntax 
+		driver.findElement(By.className("shopping_cart_link")).click();	//CSS used class	
+		driver.findElement(By.id("checkout")).click();
+		
+		driver.findElement(By.id("first-name")).sendKeys("Ivan");
+		driver.findElement(By.id("last-name")).sendKeys("Armadi");
+		driver.findElement(By.id("postal-code")).sendKeys("Jakarta");
+		driver.findElement(By.id("continue")).click();
+
+		driver.findElement(By.id("finish")).click();
+	}
+
+}
