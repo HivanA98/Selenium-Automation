d8bf8fddfdbb040ae5b48678ce01decdf235b7fe
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-09-08 19:53:49 +0700

    NewTest

---
+++ TestNG/src/SauceDemo/SauceSteps.java
@@ -0,0 +1,10 @@
+package SauceDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SauceSteps {
+	 driver = new ChromeDriver();
+     driver.get("https://www.saucedemo.com/");
+     loginPage = new LoginPage(driver);
+}
